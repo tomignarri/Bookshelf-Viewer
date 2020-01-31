ef70508f0e1d23953e006c7b76f5898d465da549
govtbodies <tom.ignarri@gmail.com>
2020-01-31 17:46:33 -0500

    set up axios

--- src/api/goodreads.js
+++ src/api/goodreads.js
@@ -1,1 +1,8 @@
 import axios from 'axios';
+
+export default axios.create({
+    baseURL: 'https://www.goodreads.com',
+    headers: {
+        Authorization: 'Client-ID '
+    }
+});
--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import goodreads from '../api/goodreads';
 import BookList from './BookList';
 import UserSearch from './UserSearch';
 
@@ -7,11 +8,11 @@ class App extends React.Component {
     state = { books: [] }
 
     onSearchSubmit = async term => {
-        // const response = await api.get('/search/books', {
-        //     params: { query: term },
-        // });
+        const response = await goodreads.get('/owned_books/user?format=xml', {
+            params: { id: term }
+        });
     
-        // this.setState({ books: response.data.results });
+        this.setState({ books: response.data.results });
     }
 
     render(){

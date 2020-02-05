5495464f588cab66202caae7999ada6f3e431449
govtbodies <tom.ignarri@gmail.com>
2020-02-05 15:19:04 -0500

    setup Cors proxy

--- src/api/goodreads.js
+++ src/api/goodreads.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
+const proxyurl = "https://cors-anywhere.herokuapp.com/";
+
 export default axios.create({
-    baseURL: 'https://www.goodreads.com',
+    baseURL: proxyurl + 'https://www.goodreads.com',
+    headers: {
+        'Access-Control-Allow-Origin': '*'
+    }
 });

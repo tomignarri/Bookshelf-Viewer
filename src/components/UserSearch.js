29dd23b70323bdcf8302300139c50aaa9af3568d
govtbodies <tom.ignarri@gmail.com>
2020-02-18 17:29:10 -0500

    update navbar style

--- src/components/UserSearch.js
+++ src/components/UserSearch.js
@@ -13,22 +13,26 @@ class UserSearch extends React.Component {
     render(){
         return (
             <div>
-                <nav className="navbar navbar-light bg-light">
+                <nav className="navbar navbar-light bg-dark justify-content-between">
+                <span class="navbar-text text-white">
+                  Bookshelf Viewer
+                </span>
                   <form onSubmit={this.onFormSubmit} className="form-inline">
+                      
                       <div>
                         <input type="text" 
                           value={this.state.term} 
                           onChange={(e) => this.setState({ term: e.target.value })} 
-                          className="form-control"
+                          className="form-control form-control-sm"
                           placeholder="Search Author" required></input>
                       </div>
                       <div>
                         <input 
                           type="submit" 
                           value="Submit" 
-                          className="btn btn-outline-success">
+                          className="btn btn-light btn-sm ml-2">
                         </input>
-                        </div>
+                      </div>
                   </form>
                 </nav>
                                
